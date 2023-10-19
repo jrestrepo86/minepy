7438c64423bca530c21e30eff16821c3b14e8760
jrestrepo <jrestrepo@ingenieria.uner.edu.ar>
2023-10-19 11:45:09 -0300

    class_diff_cmi and class_gen_cmi renamed

---
+++ tests/test_all/test_all_cmi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+from minepy.class_mi.class_diff_cmi import ClassDiffCMI
+from minepy.class_mi.class_gen_cmi import ClassGenCMI
+from minepy.gan_mi.gan_cmi import GanCMI
